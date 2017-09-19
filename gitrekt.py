d5ffb934226f2754bee563bbd197d4ee8b157726
Mike <michael.bishop17@gmail.com>
2017-09-19 14:12:54 -0700

    Update gitrekt.py

--- gitrekt.py
+++ gitrekt.py
@@ -3,10 +3,12 @@
 #cat json.txt | python -m json.tool >> pretty.txt
 #
 from requests.auth import HTTPBasicAuth
+from gitrekt_paginator import pagination
 import requests
 import json
 import argparse
 import re
+
 
 ####################################################################################################
 #Arg Parsing
@@ -21,23 +23,6 @@ gitUser = args.user
 gitPass = args.password
 gitTerm = args.term
 gitPages = 0
-####################################################################################################
-
-####################################################################################################
-#Check pagination to see how many times we need to repeat the search
-#This is bad and needs to be refactored
-def pagination(gitUser, gitPass):
-	header_text_highlite = {'Accept': 'application/vnd.github.v3.text-match+json'}
-	with requests.Session() as session:
-       		search = session.head('https://api.github.com/search/code?q='+gitTerm+'+in:file', headers=header_text_highlite, auth=HTTPBasicAuth(gitUser, gitPass))
-        	#mystring = search.headers
-        	link = search.headers.get('link', None)
-        	if link is not None:
-                	#print link
-                	m = re.search('page=(\d+.)>; rel="last"', link)
-                	print("MAX PAGES: " + m.group(1))
-			maxPages = m.group(1)
-			return maxPages
 ####################################################################################################
 
 ####################################################################################################
