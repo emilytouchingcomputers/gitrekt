a7f7b5a275d567e373a45ab89c38e8d8120f6558
Mike <michael.bishop17@gmail.com>
2017-09-19 14:22:33 -0700

    Added comments

--- gitrekt_paginator.py
+++ gitrekt_paginator.py
@@ -1,15 +1,24 @@
+#Handles figuring out how many pages of search results we get
+#Very wonky, and there has to be a better way.
+#Broke it out of gitrekt.py because it was making the code unbearable to read
 from requests.auth import HTTPBasicAuth
 import requests
 import re
+#gitUser, gitPass, gitTerm are from gitrekt.py
+#username, password, search term respectively.
 def pagination(gitUser, gitPass, gitTerm):
+	#No clue what this does but I'm scared to delete it
 	header_text_highlite = {'Accept': 'application/vnd.github.v3.text-match+json'}
 	with requests.Session() as session:
        		search = session.head('https://api.github.com/search/code?q='+gitTerm+'+in:file', headers=header_text_highlite, auth=HTTPBasicAuth(gitUser, gitPass))
-        	#mystring = search.headers
-        	link = search.headers.get('link', None)
+        	#link is current page, next page, last page
+		link = search.headers.get('link', None)
         	if link is not None:
                 	#print link
+			#regex to grab the page number of the last page of results
                 	m = re.search('page=(\d+.)>; rel="last"', link)
+			#print the number of pages (i.e. the number of the last page)
                 	print("MAX PAGES: " + m.group(1))
 			maxPages = m.group(1)
+			#return maxpages to gitrekt.py for our while loop for searching
 			return maxPages
