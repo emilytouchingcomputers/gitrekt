55b92f57b16ecfa0c15cdc75655510019bc179a1
Mike <michael.bishop17@gmail.com>
2017-09-19 14:37:56 -0700

    Update gitrekt.py
    Comments

--- gitrekt.py
+++ gitrekt.py
@@ -6,34 +6,38 @@ import argparse
 import re
 ####################################################################################################
 #Arg Parsing
-#Take user/password/search term.  Need to add error handling.
+#Take user/password/search term.
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', '--user', default = 'invalid',  type=str, help='Github Username')
 parser.add_argument('-p', '--password', default = 'invalid',  type=str, help='Github Password')
 parser.add_argument('-t', '--term', default = 'invalid', type=str, help='Search Term')
 args = parser.parse_args()
 
+#Turn our args into vars
 gitUser = args.user
 gitPass = args.password
 gitTerm = args.term
 gitPages = 0
+#This checks to make sure people filled out all of the args
 if (gitUser == 'invalid' or  gitPass == 'invalid' or gitTerm == 'invalid'):
 	parser.print_help()
 
 ####################################################################################################
 
-#Searching
-#file = open('~/Desktop/json.txt', 'w')
+#Search functionality
+#Gets the max number of results pages so we can go through all of them since github api only returns one page at a time
 gitPages = pagination(gitUser, gitPass, gitTerm)
 header_text_highlite = {'Accept': 'application/vnd.github.v3.text-match+json'}
+#While loop and counter for getting all pages of results
 counter = 0
 while (counter <= gitPages):
 	with requests.Session() as session:
 		search = session.get('https://api.github.com/search/code?q='+gitTerm+'+in:file&page=' + str(counter), headers=header_text_highlite, auth=HTTPBasicAuth(gitUser, gitPass))
-	#print search.headers
+		#Our results are stored in var data
 		data = search.text
-	#print data
+	        #Load those results into the json parser
 		j = json.loads(data)
+		#Bro I don't even know.  Wrote this code months ago and can't figure out whats going on.  JSON parsing is hard.
 		count = len(j["items"])
 		for x in range(0, count):
 			print("Repo Name: " + j['items'][x]['repository']['name'])
@@ -43,6 +47,8 @@ while (counter <= gitPages):
 			print("*************************************************************")
 			print("")
 			print("")
+		#Increase our counter to change the search page to the next one	
 		counter += 1
+		#Print what the next page of results is
 		print("NEXT PAGE: "+ str(counter))
 ####################################################################################################
