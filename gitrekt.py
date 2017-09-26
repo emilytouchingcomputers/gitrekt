7395cf7e4e3720211808478b8976ce76a472acc7
root <root@localhost.localdomain>
2017-09-26 13:27:53 -0700

    html output

--- gitrekt.py
+++ gitrekt.py
@@ -39,7 +39,7 @@ header_text_highlite = {'Accept': 'application/vnd.github.v3.text-match+json'}
 counter = 1
 #Yeah so turns out gitPages was returning a string and it was ruining this while test, so now we cast it to int because that wasted 4 hours of my life.
 gitPages = int(gitPages)
-f = open('results_' + gitTerm + "_.txt", 'w+')
+f = open('results_' + gitTerm + "_.html", 'w+')
 while (counter < gitPages+1):
 	with requests.Session() as session:
 		try:
@@ -47,38 +47,38 @@ while (counter < gitPages+1):
                 		search = session.get('https://api.github.com/search/code?q='+gitTerm+'+in:file&page=' + str(counter), headers=header_text_highlite, auth=HTTPBasicAuth(gitUser, gitPass))
                 		#Our results are stored in var data
                 		data = search.text
-				#Load the results into the JSON parser
-				j = json.loads(data)
-				#Bro I don't even know.  Wrote this code months ago and can't figure out whats going on.  JSON parsing is hard.
-				count = len(j["items"])
-				for x in range(0, count):
-					f.write("<RESULT>\n")
-					f.write("<REPO_NAME> " + j['items'][x]['repository']['name'])
-					f.write("</REPO_NAME>\n")
-					f.write("<FILENAME> " + j['items'][x]['name'])
-					f.write("</FILENAME>\n")
-					f.write("<CODE> " + j['items'][x]['text_matches'][0]['fragment'])
-					f.write("</CODE>\n")
-					f.write("<REPO_URL> " + j['items'][x]['html_url'])
-					f.write("</REPO_URL>\n")
-					m = re.search('github.com/(\w+.)/', j['items'][x]['html_url'])
-					user = m.group(1)
-					f.write("<USER>")
-					f.write(user)
-					f.write("</USER>\n")
-					f.write("</RESULT>\n")
-					f.write("\n\n")
-		except Exception:
-			#We probably caught a rate limit
-			if "abuse-rate-limits" in data:
-                        	print ("RATE LIMITED!")
-                        	print("SLEEPING 60 SECONDS")
-                        	time.sleep(60)
-                        	print("RESUMING!")
-			pass
-	#Increase our counter to change the search page to the next one	
-	counter += 1
-	print("CURRENT PAGE: "+ str(counter - 1)+ " of " + str(gitPages))
+                                #Load the results into the JSON parser
+                                j = json.loads(data)
+                                #Bro I don't even know.  Wrote this code months ago and can't figure out whats going on.  JSON parsing is hard.
+                                count = len(j["items"])
+                                for x in range(0, count):
+                                        f.write("<RESULT class='result'>\n")
+                                        f.write("<b>REPO NAME: </b> " + j['items'][x]['repository']['name'])
+                                        f.write("<br>\n")
+                                        f.write("<b>FILE NAME: </b> " + j['items'][x]['name'])
+                                        f.write("<br>\n")
+                                        f.write("<b>URL: </b><a href=\" " + j['items'][x]['html_url'])
+                                        f.write("\">LINK</a><br>\n")
+                                        m = re.search('github.com/(\w+.)/', j['items'][x]['html_url'])
+                                        user = m.group(1)
+                                        f.write("<b>USER: </b>")
+                                        f.write(user)
+                                        f.write("<br>\n")
+					f.write("<b>SNIPPET: </b><pre> " + j['items'][x]['text_matches'][0]['fragment'])
+                                        f.write("</pre><br>\n")
+                                        f.write("</RESULT>\n")
+                                        f.write("<br><br>\n\n")
+                except Exception:
+                        #We probably caught a rate limit
+                        if "abuse-rate-limits" in data:
+                                print ("RATE LIMITED!")
+                                print("SLEEPING 60 SECONDS")
+                                time.sleep(60)
+                                print("RESUMING!")
+                        pass
+        #Increase our counter to change the search page to the next one 
+        counter += 1
+        print("CURRENT PAGE: "+ str(counter - 1)+ " of " + str(gitPages))
 
 f.close()
 ####################################################################################################
