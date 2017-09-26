a6c01764e47fc041268fd369460d3878297d9b5d
root <root@localhost.localdomain>
2017-09-26 15:24:02 -0700

    bad filtering with tester.py

---
+++ tester.py
@@ -0,0 +1,9 @@
+f = open('results_gsm1900.org_.html', 'r+')
+e = open('results_filtered.html', 'w')
+d = f.readlines()
+f.seek(0)
+for line in d:
+	if "ehime" not in line:
+		e.write(line)
+f.close()
+e.close
